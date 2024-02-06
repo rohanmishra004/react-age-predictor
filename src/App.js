1fcabcec15f52c24f286bcfb367859b7862fbec5
Rohan Mishra <rohanmishra.004@gmail.com>
2024-02-07 01:09:44 +0530

    cat fact generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+
 
 function App() {
+  const [fact, setFact] = useState("")
+  const [isloading, setisLoading] = useState(true)
+
+  const fetchData = () => {
+    axios.get("https://catfact.ninja/fact?max_length=140")
+      .then(res => {
+        setFact(res.data.fact)
+        setisLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isloading && <p>Loading Page...</p>}
+      <button onClick={fetchData}>Generate Random catfacts</button>
+      {fact && <p> {fact}</p>}
+    </>
+
   );
 }
 
